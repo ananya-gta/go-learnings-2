2bd05c78406087ce6c17fcc8842f336fe9c1e678
ananya-gta <anannya.gta@gmail.com>
2024-10-19 16:55:38 +0530

    created go.mod file for bank.go and finished with the logic of bank application

--- Section-2-Go-Essentials/bank/bank.go
+++ Section-2-Go-Essentials/bank/bank.go
@@ -3,11 +3,53 @@ package main
 import "fmt"
 
 func main() {
-	printChoices()
+	var accountBalance = 1000.0
+	fmt.Println("Welcome to Go Bank!")
+	for {
+		printChoices()
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+			case 1:
+				fmt.Println("Your current balance is: ", accountBalance)
+			case 2:
+				fmt.Print("Your deposit: ")
+				var depositAmount float64
+				fmt.Scan(&depositAmount)
+				if depositAmount <= 0 {
+					fmt.Println("Minimum deposit amount is Rs. 1")
+					continue
+				}
+				accountBalance += depositAmount
+				fmt.Println("Your current balance is: ", accountBalance)
+			case 3:
+				fmt.Print("Enter withdraw amount: ")
+				var withdrawAmount float64
+				fmt.Scan(&withdrawAmount)
+				if withdrawAmount <= 0 {
+					fmt.Print("Minimum withdar amount should be Rs. 100")
+					continue
+				}
+				if withdrawAmount > accountBalance {
+					fmt.Print("Withdrawal amount should not be more than current balance. Your current balance is: ", accountBalance)
+					continue
+				}
+				accountBalance -= withdrawAmount
+				fmt.Println("Your current balance is: ", accountBalance)
+			default:
+				fmt.Print("Thankyou for visiting Go Bank !")
+				return
+		}
+
+	}
+
 }
 
 func printChoices() {
-	fmt.Println("Welcome to Go Bank!")
+
 	fmt.Println("Enter your choice from the below options.")
 	fmt.Println("1. Check Balance")
 	fmt.Println("2. Deposit Money")
