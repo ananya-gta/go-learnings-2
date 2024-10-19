7b8667599be60b92ec026af79a5652793abadab8
ananya-gta <anannya.gta@gmail.com>
2024-10-19 22:51:41 +0530

    cod ei ssplit into different files

--- Section-3-Working-with-Packages/bank.go
+++ Section-3-Working-with-Packages/bank.go
@@ -35,13 +35,7 @@ func main() {
 	startBankApplication(accountBalance)
 }
 
-func printChoices() {
-	fmt.Println("Enter your choice from the below options.")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
-}
+
 
 func startBankApplication(accountBalance float64) {
 	for {
---
+++ Section-3-Working-with-Packages/communication.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func printChoices() {
+	fmt.Println("Enter your choice from the below options.")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+}
