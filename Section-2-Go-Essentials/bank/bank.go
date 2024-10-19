008e00580ab80dd5bebc7349c9967cf4a757900f
ananya-gta <anannya.gta@gmail.com>
2024-10-19 21:42:28 +0530

    error handling

--- Section-2-Go-Essentials/bank/bank.go
+++ Section-2-Go-Essentials/bank/bank.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 import "os"
+import "errors"
 import "strconv"
 
+func readBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile("balance.txt")
 
-func readBalanceFromFile() (float64){
-	data, _ := os.ReadFile("balance.txt")
-	balance, _:= strconv.ParseFloat(string(data), 64)
-	return balance
+	if err != nil {
+		return 1000, errors.New("failed to find balance file")
+	}
+	balance, err := strconv.ParseFloat(string(data), 64)
+
+	if err != nil {
+		return 1000, errors.New("failed to parse stored balance value")
+	}
+	return balance, nil
 }
 
 func writeBalanceToFile(balance float64) {
@@ -17,7 +25,11 @@ func writeBalanceToFile(balance float64) {
 }
 
 func main() {
-	var accountBalance = readBalanceFromFile()
+	var accountBalance, err = readBalanceFromFile()
+	if err != nil {
+		fmt.Println("ERRRRRRRORR")
+		fmt.Println(err)
+	}
 	fmt.Println("Welcome to Go Bank!")
 	startBankApplication(accountBalance)
 }
@@ -39,37 +51,37 @@ func startBankApplication(accountBalance float64) {
 		fmt.Scan(&choice)
 
 		switch choice {
-			case 1:
-				fmt.Println("Your current balance is: ", accountBalance)
-			case 2:
-				fmt.Print("Your deposit: ")
-				var depositAmount float64
-				fmt.Scan(&depositAmount)
-				if depositAmount <= 0 {
-					fmt.Println("Minimum deposit amount is Rs. 1")
-					continue
-				}
-				accountBalance += depositAmount
-				fmt.Println("Your current balance is: ", accountBalance)
-				writeBalanceToFile(accountBalance)
-			case 3:
-				fmt.Print("Enter withdraw amount: ")
-				var withdrawAmount float64
-				fmt.Scan(&withdrawAmount)
-				if withdrawAmount <= 0 {
-					fmt.Print("Minimum withdar amount should be Rs. 100")
-					continue
-				}
-				if withdrawAmount > accountBalance {
-					fmt.Print("Withdrawal amount should not be more than current balance. Your current balance is: ", accountBalance)
-					continue
-				}
-				accountBalance -= withdrawAmount
-				fmt.Println("Your current balance is: ", accountBalance)
-				writeBalanceToFile(accountBalance)
-			default:
-				fmt.Print("Thankyou for visiting Go Bank !")
-				return
+		case 1:
+			fmt.Println("Your current balance is: ", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Minimum deposit amount is Rs. 1")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Your current balance is: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		case 3:
+			fmt.Print("Enter withdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Print("Minimum withdar amount should be Rs. 100")
+				continue
+			}
+			if withdrawAmount > accountBalance {
+				fmt.Print("Withdrawal amount should not be more than current balance. Your current balance is: ", accountBalance)
+				continue
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("Your current balance is: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		default:
+			fmt.Print("Thankyou for visiting Go Bank !")
+			return
 		}
 	}
 }
