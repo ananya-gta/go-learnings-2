1bb6f766c51295f4d310a800014f6913b77eaeee
ananya-gta <anannya.gta@gmail.com>
2024-10-23 16:06:05 +0530

    using struct tags, add meta data in struct fields

--- Section-5-Structs-and-Custom-Types/note-taking-app/noteStruct/noteStruct.go
+++ Section-5-Structs-and-Custom-Types/note-taking-app/noteStruct/noteStruct.go
@@ -10,9 +10,9 @@ import (
 )
 
 type NoteStruct struct {
-	Title     string
-	Content   string
-	CreatedAt time.Time
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // constructor function
