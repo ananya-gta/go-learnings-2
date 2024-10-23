5fa191f245eadff2d03348c69dd46e422d5802db
ananya-gta <anannya.gta@gmail.com>
2024-10-23 10:54:35 +0530

    working with long text sing buffio - Updated getUserInput function to use bufio.NewReader for reading user input, and added error handling and string trimming.

--- Section-5-Structs-and-Custom-Types/note-taking-app/main.go
+++ Section-5-Structs-and-Custom-Types/note-taking-app/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 import "example.com/note-taking-app/noteStruct"
+import "bufio"
+import "os"
+import "strings"
 
 func main() {
 	title, content := getNoteData() 
@@ -17,9 +20,22 @@ func main() {
 
 func getUserInput(prompt string) (string) {
 	fmt.Println(prompt)
-	var value string
-	fmt.Scanln(&value)
-	return value
+	// var value string
+	// fmt.Scanln(&value) 
+	// the above code will display only the first word, it cannot display long sapce separated text
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err :=reader.ReadString('\n')
+	// reader.ReadString('\n') will return a string with a newline character at the end
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
 
 func getNoteData() (string, string) {
