9a69372d0a0a07641bb66f0160008eedeb3e048a
ananya-gta <anannya.gta@gmail.com>
2024-10-23 11:00:10 +0530

    "Uisng Marshal : Added Save method to NoteStruct to save notes to file in JSON format, and updated New constructor and DisplayNote method with minor formatting changes."

--- Section-5-Structs-and-Custom-Types/note-taking-app/noteStruct/noteStruct.go
+++ Section-5-Structs-and-Custom-Types/note-taking-app/noteStruct/noteStruct.go
@@ -3,9 +3,11 @@ package noteStruct
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
+	"encoding/json"
 )
-
 
 type NoteStruct struct {
 	title     string
@@ -13,17 +15,27 @@ type NoteStruct struct {
 	createdAt time.Time
 }
 
-
 // constructor function
 func New(title, content string) (NoteStruct, error) {
 	if title == "" || content == "" {
 		return NoteStruct{}, errors.New("invalid input")
 	}
 	return NoteStruct{
-		title: title,
-		content: content,
+		title:     title,
+		content:   content,
 		createdAt: time.Now(),
 	}, nil
+}
+
+func (note NoteStruct) Save() (error){
+	fileName := strings.ReplaceAll(note.title, " ", "_")
+	fileName = strings.ToLower(fileName)
+	json, err := json.Marshal(note)
+	if err != nil {
+		return err
+	}
+	os.WriteFile(fileName, json, 0644)
+
 }
 
 func (note NoteStruct) DisplayNote() {
