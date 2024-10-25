8b157e9e8fcf233d13ae807bad2b194d04a14ab9
ananya-gta <anannya.gta@gmail.com>
2024-10-25 20:12:44 +0530

    palying with slices

--- Section-7-Arrays-Slices-Maps/arrays/arrays.go
+++ Section-7-Arrays-Slices-Maps/arrays/arrays.go
@@ -1,14 +1,24 @@
 package main
 import "fmt"
 
-func array() {
+func main() {
 	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+
+	fmt.Println(productNames)
 
 	productNames[2] = "A Carpet"
 
 	fmt.Println(productNames)
 
 	fmt.Println(prices[2])
+
+	fmt.Println(prices[1:3])
+	fmt.Println(prices[:3])
+
+	featuredPrices := prices[1:]
+	featuredPrices[0] = 199.99
+	highlightedPrices := featuredPrices[:1]
+	fmt.Println(highlightedPrices)
 }
