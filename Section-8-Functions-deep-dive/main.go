4d6ca860f66a78426a226cf0360a9e00afdd1954
ananya-gta <anannya.gta@gmail.com>
2024-10-27 14:55:51 +0530

    returning functions as value

--- Section-8-Functions-deep-dive/main.go
+++ Section-8-Functions-deep-dive/main.go
@@ -7,16 +7,22 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	numbers2 := []int{7, 2, 3, 4, 5}
+	moreNumbers := []int{7, 2, 3, 4, 5}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println("After doubling: \n", doubled)
 	fmt.Println("After tripling: \n", tripled)
 
-	transformerFunc1 := transformNumbers(&numbers, getTransformedFunc(&numbers))
-	transformerFunc2 := transformNumbers(&numbers, getTransformedFunc(&numbers2))
-	fmt.Println(transformerFunc1)
-	fmt.Println(transformerFunc2)
+	//calling the function which returns a function
+	transformFun1 := getTransformedFunc(&numbers)
+	transformFun2 := getTransformedFunc(&moreNumbers)
+
+	//gettting the new array by passing an array and function to the function
+	tranformNumbers := transformNumbers(&numbers, transformFun1)
+	tranformMoreNumbers := transformNumbers(&moreNumbers, transformFun2)
+	
+	fmt.Println(tranformNumbers)
+	fmt.Println(tranformMoreNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
