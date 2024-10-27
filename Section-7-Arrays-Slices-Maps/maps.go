7071a457c428216d834a3c5303df1059fdf26c03
ananya-gta <anannya.gta@gmail.com>
2024-10-27 11:50:47 +0530

    intoduction to maps

--- Section-7-Arrays-Slices-Maps/maps.go
+++ Section-7-Arrays-Slices-Maps/maps.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	websites := map[string]string{
+		"Google": "https://www.google.com",
+		"Facebook": "https://www.facebook.com",
+	}
 
+	fmt.Println(websites)
+
+	websites["LinkedIn"] = "https://www.linkedin.com/in"
+	fmt.Println(websites)
+
+	// delete a key-value pair from map
+	delete(websites, "Facebook")
+	fmt.Println(websites)
 }
