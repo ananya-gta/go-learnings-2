5dc73df8e617bf68da0ac17fbdb35320ccfd65e8
ananya-gta <anannya.gta@gmail.com>
2024-10-27 14:53:10 +0530

    returning functions as values

--- Section-8-Functions-deep-dive/main.go
+++ Section-8-Functions-deep-dive/main.go
@@ -7,10 +7,16 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
-	doubled := transformNumbers(&numbers, double) 
-	tripled := transformNumbers(&numbers, triple) 
-	fmt.Println("After doubling: \n", doubled)             
-	fmt.Println("After tripling: \n", tripled)             
+	numbers2 := []int{7, 2, 3, 4, 5}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println("After doubling: \n", doubled)
+	fmt.Println("After tripling: \n", tripled)
+
+	transformerFunc1 := transformNumbers(&numbers, getTransformedFunc(&numbers))
+	transformerFunc2 := transformNumbers(&numbers, getTransformedFunc(&numbers2))
+	fmt.Println(transformerFunc1)
+	fmt.Println(transformerFunc2)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -18,7 +24,7 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	for _, number := range *numbers {
 		transformNumbers = append(transformNumbers, transform(number))
 	}
-	return transformNumbers 
+	return transformNumbers
 }
 
 func double(number int) int {
@@ -29,9 +35,11 @@ func triple(number int) int {
 	return number * 3
 }
 
-
-
-
-
-
-
+// when you execute the below function you get a func as returned value
+func getTransformedFunc(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
