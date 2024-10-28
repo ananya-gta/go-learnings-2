6e8409af2b7a214abcd6c3f2bb7996af15e6c962
ananya-gta <anannya.gta@gmail.com>
2024-10-28 23:15:52 +0530

    created main.go for practive project

---
+++ Section-9-Practice-Project-Price-Calculator/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRate := []float64{0, 0.1, 0.7, 0.11}
+
+	// initialize an empty map to store the result
+	result := make(map[float64]float64)
+	
+}
