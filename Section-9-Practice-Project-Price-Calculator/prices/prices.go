6b614c8c62c2d66f4d4efc1f9b995cc92e48d2d1
ananya-gta <anannya.gta@gmail.com>
2024-10-29 12:29:42 +0530

    "Refactor price calculator to use separate package and struct for tax included price job"

--- Section-9-Practice-Project-Price-Calculator/prices/prices.go
+++ Section-9-Practice-Project-Price-Calculator/prices/prices.go
@@ -1,14 +1,24 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
-	TaxRate float64
-	InputPrices []float64
+	TaxRate           float64
+	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
 
-func  NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		TaxRate: taxRate,
+		TaxRate:     taxRate,
 		InputPrices: []float64{10, 20, 30},
 	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+	fmt.Println(result)
 }
