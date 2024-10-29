37edbff3cd9cfa29ee0125cf244931398aef07ad
ananya-gta <anannya.gta@gmail.com>
2024-10-29 11:06:37 +0530

    "Added import statement, modified taxRate variable to taxRates, and implemented logic to calculate tax-included prices and store them in a map."

--- Section-9-Practice-Project-Price-Calculator/main.go
+++ Section-9-Practice-Project-Price-Calculator/main.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	prices := []float64{10, 20, 30}
-	taxRate := []float64{0, 0.1, 0.7, 0.11}
+	taxRates := []float64{0, 0.1, 0.7, 0.11}
 
 	// initialize an empty map to store the result
-	result := make(map[float64]float64)
-	
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+	fmt.Println(result)
 }
