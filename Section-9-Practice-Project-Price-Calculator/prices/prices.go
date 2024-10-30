40ba5334bc25c60ff4aae05fcab69a1c55d88f44
ananya-gta <anannya.gta@gmail.com>
2024-10-30 16:33:32 +0530

    reading data from a txt file

--- Section-9-Practice-Project-Price-Calculator/prices/prices.go
+++ Section-9-Practice-Project-Price-Calculator/prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"buffio"
+	"fmt"
+	"go/scanner"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -21,4 +26,24 @@ func (job TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
 	}
 	fmt.Println(result)
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("an eror occured while opening the file: ", err)
+		return
+	}
+	scanner := buffio.NewScanner(file)
+	var lines []string
+    for	scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("error occured while reading the file: ", err)
+		file.Close()
+		return
+	}
 }
