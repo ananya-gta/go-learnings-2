1622b03b39ddaec08b0629c91468f128990fe41c
ananya-gta <anannya.gta@gmail.com>
2024-11-21 23:17:27 +0530

    "Added Gin server setup and GET endpoint for /events, and created getEvents function"

--- Section-11-Rest-API-Project/main.go
+++ Section-11-Rest-API-Project/main.go
@@ -1,7 +1,15 @@
-package main 
+package main
 
 import "github.com/gin-gonic/gin"
 
 func main() {
-	gin.Default()
+	server := gin.Default()
+
+	server.GET("/events", )
+
+	server.Run(":8080")
+}
+
+func getEvents(c *gin.Context) {
+	
 }
