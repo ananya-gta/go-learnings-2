3a1915251869873902813c97641c95f79b69a1a5
ananya-gta <anannya.gta@gmail.com>
2024-11-22 00:19:15 +0530

    created addEvents api

--- Section-11-Rest-API-Project/main.go
+++ Section-11-Rest-API-Project/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/building-a-rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +11,27 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/events", getEvents)
+	router.POST("/addEvents", addEvent)
 
 	router.Run(":8080")
 }
 
 func getEvents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message":"Hello"})
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, events)
+}
+
+func addEvent(c *gin.Context) {
+	var event models.Event
+
+	err := c.ShouldBindJSON(&event) // maps the JSON response data directly to the struct
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	c.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
--- Section-11-Rest-API-Project/models/event.go
+++ Section-11-Rest-API-Project/models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID      int
 }
 
@@ -20,4 +20,8 @@ func (e Event) Save() {
 
 func GetAllEvents() []Event {
 	return events
+}
+
+func AddEvent(e Event) {
+	events = append(events, e)
 }
