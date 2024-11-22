0065f48f8fc2bc0e97cf47c9d84b8ac9b5618515
ananya-gta <anannya.gta@gmail.com>
2024-11-22 19:20:55 +0530

    added comments to understand how data is inserted in db

--- Section-11-Rest-API-Project/models/event.go
+++ Section-11-Rest-API-Project/models/event.go
@@ -8,36 +8,38 @@ import (
 
 type Event struct {
 	ID          int64
-	Name        string    `binding:"required"`
-	Description string    `binding:"required"`
-	Location    string    `binding:"required"`
-	DateTime    time.Time 
+	Name        string `binding:"required"`
+	Description string `binding:"required"`
+	Location    string `binding:"required"`
+	DateTime    time.Time
 	UserID      int
 }
 
 var events = []Event{}
 
-func (e Event) Save() error{
+func (e Event) Save() error {
 	// add it to database
 	insertQuery := `
 	INSERT INTO events (name, description, location, datetime, user_id)
 	VALUES (?, ?, ?, ?, ?)
 	`
-
+	// Prepare the SQL insert query
 	stmt, err := db.DB.Prepare(insertQuery)
 
 	if err != nil {
 		return err
 	}
 
+	// Make sure to close the statement when the function exits
 	defer stmt.Close()
 
-	// execute the insert query
+	// Execute the insert query with the values from the Event struct
 	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
 	if err != nil {
 		return err
 	}
 
+	// Retrieve the ID of the newly inserted row
 	id, err := result.LastInsertId()
 	e.ID = id
 
